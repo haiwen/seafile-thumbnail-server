502fe7dd132fba8329802797acae7f97a581ec52
r350178982 <32759763+r350178982@users.noreply.github.com>
2024-09-13 16:07:34 +0800

    Update seahub_api.py

--- seafile_thumbnail/seahub_api.py
+++ seafile_thumbnail/seahub_api.py
@@ -14,6 +14,8 @@ def get_jwt_url(repo_id):
     return jwt_url
 
 
+
+
 def jwt_permission_check(session_key, repo_id, path):
     jwt_url = get_jwt_url(repo_id)
     payload = {
@@ -21,10 +23,38 @@ def jwt_permission_check(session_key, repo_id, path):
     }
     jwt_token = jwt.encode(payload, JWT_PRIVATE_KEY, algorithm='HS256')
     headers = {
-        'Authorization': f'token {jwt_token}'
+        'Authorization': f'token {jwt_token}',
+        'Cookie': "sessionid=%s" % session_key
     }
     try:
-        response = requests.post(jwt_url, data={'path': path}, headers=headers, cookies={'session_key': session_key})
+        response = requests.post(jwt_url, data={'path': path}, headers=headers)
+        if response.status_code != 200:
+            error_msg = 'Internal Server Error'
+            logger.error(error_msg)
+            return False
+
+        res = json.loads(response.text)
+        if res["success"]:
+            return True
+        else:
+            return False
+    except Exception as e:
+        logger.error(e)
+        return False
+    
+def jwt_share_link_permission_check(session_key, token):
+    jwt_url = '%s/api/v2.1/internal/check-share-link-thumbnail/' % SEAHUB_SERVICE_URL.rstrip('/')
+    
+    payload = {
+        'is_internal': True
+    }
+    jwt_token = jwt.encode(payload, JWT_PRIVATE_KEY, algorithm='HS256')
+    headers = {
+        'Authorization': f'token {jwt_token}',
+        'Cookie': "sessionid=%s" % session_key
+    }
+    try:
+        response = requests.post(jwt_url, data={'token': token}, headers=headers)
         if response.status_code != 200:
             error_msg = 'Internal Server Error'
             logger.error(error_msg)
