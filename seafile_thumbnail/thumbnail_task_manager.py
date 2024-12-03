db2fac0269ed3238f51737adcb4a446a743f6801
孙永强 <11704063+s-yongqiang@user.noreply.gitee.com>
2024-12-03 15:51:55 +0800

    update

--- seafile_thumbnail/thumbnail_task_manager.py
+++ seafile_thumbnail/thumbnail_task_manager.py
@@ -13,6 +13,7 @@ class ThumbnailManager(object):
         self.tasks_map = {}
         self.task_results_map = {}
         self.image_queue = queue.Queue(32)
+        self.video_queue = queue.Queue(32)
         self.current_task_info = {}
         self.threads = []
 
@@ -62,14 +63,14 @@ class ThumbnailManager(object):
         return task_id, 200
 
     def add_video_task(self, func, repo, file_id, path, size, thumbnail_file):
-        if self.image_queue.full():
+        if self.video_queue.full():
             logger.warning('thumbnail server busy, queue size: %d, current tasks: %s, threads is_alive: %s'
                             % (self.image_queue.qsize(), self.current_task_info,
                             self.threads_is_alive()))
             return ('thumbnail server busy.', 400)
         task_id = str(uuid.uuid4())
         task = (func, (repo, file_id, path, size, thumbnail_file))
-        self.image_queue.put(task_id)
+        self.video_queue.put(task_id)
         self.tasks_map[task_id] = task
         return task_id, 200
 
@@ -116,13 +117,50 @@ class ThumbnailManager(object):
             finally:
                 self.tasks_map.pop(image_id, None)
 
+    def handle_video_task(self):
+        while True:
+            try:
+                video_id = self.video_queue.get(timeout=2)
+            except queue.Empty:
+                continue
+            except Exception as e:
+                logger.error(e)
+                continue
+            task = self.tasks_map.get(video_id)
+            if type(task) != tuple or len(task) < 1:
+                continue
+            task_info = video_id + ' ' + str(task[0])
+            try:
+                self.current_task_info[video_id] = task_info
+                logging.info('Run task: %s' % task_info)
+                start_time = time.time()
+                # run
+                task[0](*task[1])
+                self.task_results_map[video_id] = 'success'
+
+                finish_time = time.time()
+                logging.info('Run task success: %s cost %ds \n' % (task_info, int(finish_time - start_time)))
+                self.current_task_info.pop(video_id, None)
+            except Exception as e:
+                self.task_results_map[video_id] = 'error_' + str(e.args[0])
+                logger.exception(e)
+                logger.error('Failed to handle task %s, error: %s \n' % (task_info, e))
+                self.current_task_info.pop(video_id, None)
+            finally:
+                self.tasks_map.pop(video_id, None)
+
     def run(self, task_workers=3):
         image_name = 'ImageManager Thread-'
+        video_name = 'VideoManager Thread-'
         for thread_num in range(task_workers):
             image_t = threading.Thread(target=self.handle_image_task, name=image_name+str(thread_num))
+            video_t = threading.Thread(target=self.handle_video_task, name=video_name+str(thread_num))
             image_t.setDaemon(True)
+            video_t.setDaemon(True)
             image_t.start()
+            video_t.start()
             self.threads.append(image_t)
+            self.threads.append(video_t)
 
 
 thumbnail_task_manager = ThumbnailManager()
