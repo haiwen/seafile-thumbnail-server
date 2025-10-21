aaf86fec28f6a6385c2872649db35885492a5d18
SkywalkerSpace <dzmbbs@qq.com>
2025-10-21 13:14:35 +0800

    update build.sh

--- docker/docker-build-push.sh
+++ docker/docker-build-push.sh
@@ -6,4 +6,8 @@ docker build -t docker.seafile.top/seafileltd/thumbnail-server:${version}-testin
 
 docker tag docker.seafile.top/seafileltd/thumbnail-server:${version}-testing seafileltd/thumbnail-server:${version}-testing
 
+docker push seafileltd/thumbnail-server:${version}-testing
 docker push docker.seafile.top/seafileltd/thumbnail-server:${version}-testing
+
+echo seafileltd/thumbnail-server:${version}-testing
+echo docker.seafile.top/seafileltd/thumbnail-server:${version}-testing
