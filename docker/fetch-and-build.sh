166dce3893eedfab95ee010b3b9c9b0d524fec7c
SkywalkerSpace <dzmbbs@qq.com>
2025-10-21 12:03:24 +0800

    update fetch-andbuild.sh

--- docker/fetch-and-build.sh
+++ docker/fetch-and-build.sh
@@ -3,8 +3,10 @@
 git pull
 
 cd thumbnail-server/
-
 cp -r /opt/seafile-thumbnail-server/seafile_thumbnail .
 cp /opt/seafile-thumbnail-server/app.py .
 cp /opt/seafile-thumbnail-server/main.py .
 cp /opt/seafile-thumbnail-server/requirements.txt .
+
+cd ../seafobj
+git pull
