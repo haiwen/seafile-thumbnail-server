5a738d837a7faf848419ecbdf705e24fa6fd170f
SkywalkerSpace <dzmbbs@qq.com>
2025-10-21 12:01:11 +0800

    add fetch-and-build.sh

---
+++ docker/fetch-and-build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+git pull
+
+cd thumbnail-server/
+
+cp -r /opt/seafile-thumbnail-server/seafile_thumbnail .
+cp /opt/seafile-thumbnail-server/app.py .
+cp /opt/seafile-thumbnail-server/main.py .
+cp /opt/seafile-thumbnail-server/requirements.txt .
