e5f6dbaf01f2eef7ebb74ea526051615a0e79f12
SkywalkerSpace <dzmbbs@qq.com>
2025-11-05 16:42:34 +0800

    update docker-manifest-push.sh

--- docker/docker-manifest-push.sh
+++ docker/docker-manifest-push.sh
@@ -4,21 +4,39 @@ version=$1
 first_num=$(echo $version | cut -d '.' -f1)
 second_num=$(echo $version | cut -d '.' -f2)
 
-docker tag docker.seafile.top/seafileltd/thumbnail-server:${version}-testing docker.seafile.top/seafileltd/thumbnail-server:${version}
-docker tag docker.seafile.top/seafileltd/thumbnail-server:${version}-testing docker.seafile.top/seafileltd/thumbnail-server:${first_num}.${second_num}-latest
+docker manifest rm seafileltd/thumbnail-server:${first_num}.${second_num}-latest
 
-docker push docker.seafile.top/seafileltd/thumbnail-server:${version}
-docker push docker.seafile.top/seafileltd/thumbnail-server:${first_num}.${second_num}-latest
+docker manifest create seafileltd/thumbnail-server:${first_num}.${second_num}-latest seafileltd/thumbnail-server:${version}-testing seafileltd/thumbnail-server:${version}-arm-testing
 
-
-docker tag seafileltd/thumbnail-server:${version}-testing seafileltd/thumbnail-server:${version}
-docker tag seafileltd/thumbnail-server:${version}-testing seafileltd/thumbnail-server:${first_num}.${second_num}-latest
-
-docker push seafileltd/thumbnail-server:${version}
-docker push seafileltd/thumbnail-server:${first_num}.${second_num}-latest
+docker manifest push seafileltd/thumbnail-server:${first_num}.${second_num}-latest
 
 
-echo docker.seafile.top/seafileltd/thumbnail-server:${version}
-echo docker.seafile.top/seafileltd/thumbnail-server:${first_num}.${second_num}-latest
-echo seafileltd/thumbnail-server:${version}
+
+docker manifest rm seafileltd/thumbnail-server:${version}
+
+docker manifest create seafileltd/thumbnail-server:${version} seafileltd/thumbnail-server:${version}-testing seafileltd/thumbnail-server:${version}-arm-testing
+
+docker manifest push seafileltd/thumbnail-server:${version}
+
+
+
+docker manifest rm docker.seafile.top/seafileltd/thumbnail-server:${first_num}.${second_num}-latest
+
+docker manifest create docker.seafile.top/seafileltd/thumbnail-server:${first_num}.${second_num}-latest docker.seafile.top/seafileltd/thumbnail-server:${version}-testing docker.seafile.top/seafileltd/thumbnail-server:${version}-arm-testing
+
+docker manifest push docker.seafile.top/seafileltd/thumbnail-server:${first_num}.${second_num}-latest
+
+
+
+docker manifest rm docker.seafile.top/seafileltd/thumbnail-server:${version}
+
+docker manifest create docker.seafile.top/seafileltd/thumbnail-server:${version} docker.seafile.top/seafileltd/thumbnail-server:${version}-testing docker.seafile.top/seafileltd/thumbnail-server:${version}-arm-testing
+
+docker manifest push docker.seafile.top/seafileltd/thumbnail-server:${version}
+
+
+
 echo seafileltd/thumbnail-server:${first_num}.${second_num}-latest
+echo seafileltd/thumbnail-server:${version}
+echo docker.seafile.top/seafileltd/thumbnail-server:${first_num}.${second_num}-latest
+echo docker.seafile.top/seafileltd/thumbnail-server:${version}
