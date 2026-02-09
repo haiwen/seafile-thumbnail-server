f231f15d9c6670ed46ae2a6bc967a8288a9a78fe
Ranjiwei <32759763+r350178982@users.noreply.github.com>
2026-02-09 14:02:48 +0800

    remove-config-to-settings

--- main.py
+++ main.py
@@ -7,48 +7,9 @@ import argparse
 from app import app
 from seafile_thumbnail.thumbnail_task_manager import thumbnail_task_manager
 from threading import Thread
-from seafile_thumbnail.settings import LOG_DIR, TASK_WORKERS
+from seafile_thumbnail.settings import LOG_DIR, TASK_WORKERS, UVICORN_LOG_CONFIG
 
 # Custom uvicorn log config with timestamps
-UVICORN_LOG_CONFIG = {
-    "version": 1,
-    "disable_existing_loggers": False,
-    "formatters": {
-        "default": {
-            "()": "uvicorn.logging.DefaultFormatter",
-            "fmt": "[%(asctime)s] %(levelprefix)s %(message)s",
-            "datefmt": "%Y-%m-%d %H:%M:%S",
-            "use_colors": False,
-        },
-        "access": {
-            "()": "uvicorn.logging.AccessFormatter",
-            "fmt": "[%(asctime)s] %(levelprefix)s %(client_addr)s - \"%(request_line)s\" %(status_code)s",
-            "datefmt": "%Y-%m-%d %H:%M:%S",
-            "use_colors": False,
-        },
-    },
-    "handlers": {
-        "default": {
-            "formatter": "default",
-            "class": "logging.StreamHandler",
-            "stream": "ext://sys.stderr",
-        },
-        "access": {
-            "formatter": "access",
-            "class": "logging.StreamHandler",
-            "stream": "ext://sys.stdout",
-        },
-    },
-    "loggers": {
-        "uvicorn": {"handlers": ["default"], "level": "INFO"},
-        "uvicorn.error": {"level": "INFO"},
-        "uvicorn.access": {
-            "handlers": ["access"],
-            "level": "INFO",
-            "propagate": False,
-        },
-    },
-}
 
 class ThumbnailServer(Thread):
 
