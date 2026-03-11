8413dbc6a45ebf00296935ed09a8406b06de04a2
Ranjiwei <32759763+r350178982@users.noreply.github.com>
2026-03-11 14:02:52 +0800

    opt-remove-unnecessary-format

--- seafile_thumbnail/utils.py
+++ seafile_thumbnail/utils.py
@@ -6,7 +6,7 @@ import jwt
 import time
 from seafile_thumbnail.settings import JWT_PRIVATE_KEY
 from seafile_thumbnail.constants import TEXT, IMAGE, DOCUMENT, SPREADSHEET, SVG, PDF, MARKDOWN, VIDEO, \
-    AUDIO, XMIND, SEADOC, RAW, EXR, EPS, TEXT_PREVIEW_EXT
+    AUDIO, XMIND, SEADOC, TEXT_PREVIEW_EXT
 from sqlalchemy import text
 from seafobj import fs_mgr, commit_mgr
 from seafile_thumbnail.db import init_db_session_class
@@ -22,9 +22,6 @@ PREVIEW_FILEEXT = {
     AUDIO: ('mp3', 'oga', 'ogg', 'wav', 'flac', 'opus'),
     XMIND: ('xmind',),
     SEADOC: ('sdoc',),
-    RAW: ('cr2', 'cr3', 'nef', 'arw', 'dng'),
-    EXR: ('exr',),
-    EPS: ('eps',),
 }
 ZERO_OBJ_ID = '0000000000000000000000000000000000000000'
 LARGE_FILE_THRESHOLD = 1024 * 1024 * 1024 # 1GB, trigger garbage collection for large file.
