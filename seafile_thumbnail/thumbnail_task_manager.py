67cd23767961c6c7f1fa96cd59a065a87746f6ee
Ranjiwei <32759763+r350178982@users.noreply.github.com>
2026-06-18 14:19:22 +0800

    update

--- seafile_thumbnail/thumbnail_task_manager.py
+++ seafile_thumbnail/thumbnail_task_manager.py
@@ -309,7 +309,8 @@ class ThumbnailManager(object):
                     self._set_result(pdf_id, 'error_' + str(e.args[0]))
                 else:
                     self._set_result(pdf_id, 'error_' + str(e))
-                logger.exception('Failed to handle task %s, error: %s \n' % (task_info, e))
+                log_func = logger.warning if isinstance(e, FileInvalidError) else logger.exception
+                log_func('Failed to handle task %s, error: %s \n' % (task_info, e))
                 self.current_task_info.pop(pdf_id, None)
             finally:
                 self.tasks_map.pop(pdf_id, None)
